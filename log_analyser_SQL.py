1fd1f12fce02e9ff944e933386fe774612d2b768
Ashwini Patil <ashwini130297@gmail.com>
2020-10-15 16:49:32 +0530

    Update log_analyser_SQL.py

--- log_analyser_SQL.py
+++ log_analyser_SQL.py
@@ -16,12 +16,12 @@ table = table.map(lambda line: (line[0],line[1]))
 table = table.map(lambda line: (line[0],line[1].split(":")))
 tabledf1 = table.map(lambda line : (line[0],line[1][0],line[1][1]))
 tabledf1 = tabledf1.map(lambda line:(line[0],line[1],line[2].split("(execution took"))).filter(lambda line: len(line[2])==2)
-tabledf2 = tabledf1.map(lambda line:(line[0],line[1],line[2][0],line[2][1])).filter(lambda line: len(line)==4)
+tabledf1 = tabledf1.map(lambda line:(line[0],line[1],line[2][0],line[2][1])).filter(lambda line: len(line)==4)
 
 #Call an action to execute the job
-print('The total number of rows is:',tabledf2.count())
+print('The total number of rows is:',tabledf1.count())
 #print(tabledf2.count())
 
-#tableDFnew = spark.createDataFrame(tabledf2)
+tableDF = spark.createDataFrame(tabledf1)
 
-#tableDFnew.write.format('csv').option('header',True).mode('overwrite').save('file:///F:/Hadoop/sparkjobtest.csv')
+tableDF.write.format('csv').option('header',True).mode('overwrite').save('file:///F:/Hadoop/sparkjobtest.csv')
